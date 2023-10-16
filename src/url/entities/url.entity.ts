675747bd268c1484facc1550ffd88a308fa4607b
theo-code33 <gillettheo1@gmail.com>
2023-10-16 14:03:07 +0200

    create url entity

--- src/url/entities/url.entity.ts
+++ src/url/entities/url.entity.ts
@@ -1,1 +1,26 @@
-export class Url {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Url {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  baseUrl: string;
+  @Column()
+  slug: string;
+  @Column()
+  clicks: number;
+  @CreateDateColumn()
+  createdAt: Date;
+  @UpdateDateColumn()
+  updatedAt?: Date;
+  @DeleteDateColumn()
+  deletedAt?: Date;
+}
