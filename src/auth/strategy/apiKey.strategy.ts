532e85682e8658c11c9ef90a6647122379014eb0
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-10-17 23:21:15 +0200

    add apiToken strategy and jwt strategy

---
+++ src/auth/strategy/apiKey.strategy.ts
@@ -0,0 +1,24 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import Strategy from 'passport-headerapikey';
+import { ApiKeyService } from 'src/api-key/api-key.service';
+
+@Injectable()
+export class ApiKeyStrategy extends PassportStrategy(Strategy, 'api-key') {
+  constructor(private readonly apiKeyService: ApiKeyService) {
+    super({ header: 'X-API-KEY', prefix: '' }, true, async (apiKey, done) => {
+      return this.validateApiKey(apiKey, done);
+    });
+  }
+
+  async validateApiKey(apiKey: string, done: (error: Error, data) => void) {
+    const apiKeyResponse = await this.apiKeyService.findByKey(apiKey);
+    if (!apiKeyResponse) {
+      return done(
+        new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED),
+        null,
+      );
+    }
+    return done(null, apiKeyResponse);
+  }
+}
